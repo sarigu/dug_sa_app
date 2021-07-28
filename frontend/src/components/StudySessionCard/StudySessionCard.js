4b4e51f42f07642f95ecac464961d93b5c3e7f61
Sari <sariguci@Saris-MacBook-Pro.local>
2021-07-28 02:40:32 +0200

    updated date display

--- frontend/src/components/StudySessionCard/StudySessionCard.js
+++ frontend/src/components/StudySessionCard/StudySessionCard.js
@@ -4,11 +4,21 @@ import './StudySessionCard.css';
 
 const StudySessionCard = (props) => {
 
+    const [formattedDate, setFormattedDate] = useState();
+
+    useEffect(() => {
+            if (props.date) {
+                let [year, month, day] = props.date.split('-');
+                let date = day + "." + month + "." + year;
+                setFormattedDate(date);
+            }
+    }, [props]);
+
     return (
         <div className={props.isActive ? "reminder-card" : "reminder-card cancelled-card"} style={{ backgroundColor: `${props.subjectColor}` }}>
             {!props.isActive ? <div className="cancelled-heading-wrapper"><strong>Cancelled</strong><span>&#128680;</span></div> : null}
             <strong>{props.subject + " with " + props.teacher.first_name + " " + props.teacher.last_name}</strong>
-            <p>{props.startTime + " - " + props.endTime + " on " + props.date}</p>
+            <p>{props.startTime + " - " + props.endTime + " on " + formattedDate}</p>
             <p>{"at " + props.location}</p>
         </div>
     );
