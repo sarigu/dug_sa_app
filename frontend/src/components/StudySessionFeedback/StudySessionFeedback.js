c111f264a40b5e68aa715c331674c2536341d4e3
Sari <sariguci@Saris-MacBook-Pro.local>
2021-07-30 02:26:20 +0200

    fixed redirect issue after feedback msg

--- frontend/src/components/StudySessionFeedback/StudySessionFeedback.js
+++ frontend/src/components/StudySessionFeedback/StudySessionFeedback.js
@@ -5,55 +5,48 @@ import { useHistory, useLocation } from "react-router-dom";
 import './StudySessionFeedback.css';
 
 const BookingSuccessMessage = (props) => {
-    const history = useHistory();
-    const location = useLocation();
 
     return (
         <div className="feedback-content">
             <span className="feedback-icon" >&#127881;</span>
             <h3>You successfully booked your spot!</h3>
-            <button onClick={() => { location.pathname = "/dashboard" ? props.selectedCallback() : history.push("/dashboard") }}>Go back</button>
+            <button onClick={() => { window.location.href = "/dashboard" }}>Go back</button>
         </div>
     );
 };
 
 const FailMessage = (props) => {
-    const history = useHistory();
-    const location = useLocation();
+
     return (
         <div className="feedback-content">
             <span className="feedback-icon" >&#127871;</span>
             <h3>Ooops, something went wrong! <br />Please try again later</h3>
-            <button onClick={() => { console.log("LOCATION", location, location.pathname); location.pathname = "/dashboard" ? props.selectedCallback() : history.push("/dashboard") }}>Go back</button>
+            <button onClick={() => { window.location.href = "/dashboard" }}>Go back</button>
         </div>
     );
 };
 
 const DropOutOfStudySessionSuccessMessage = (props) => {
-    const history = useHistory();
-    const location = useLocation();
+
 
     return (
         <div className="feedback-content">
             <span className="feedback-icon" >&#9989;</span>
             <h3>Your spot in the study session is cancelled!</h3>
-            <button onClick={() => { location.pathname = "/dashboard" ? props.selectedCallback() : history.push("/dashboard") }}>Go back</button>
+            <button onClick={() => { window.location.href = "/dashboard" }}>Go back</button>
         </div>
     );
 };
 
 
 const CancelStudySessionSuccessMessage = (props) => {
-    const history = useHistory();
-    const location = useLocation();
 
-    console.log(location.pathname)
 
     return (
         <div className="feedback-content">
             <span className="feedback-icon" >&#9989;</span>
             <h3>The study session is cancelled!</h3>
-            <button onClick={() => { location.pathname = "/dashboard" ? props.selectedCallback() : history.push("/dashboard") }}>Go back</button>
+            <button onClick={() => { window.location.href = "/dashboard" }}>Go back</button>
         </div>
     );
 };
