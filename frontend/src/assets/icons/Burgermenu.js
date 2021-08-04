dff1b74ae59adc0dd1acf9f66a58dce564809926
Sari <sariguci@Saris-MacBook-Pro.local>
2021-08-04 19:16:04 +0200

    updated gitignore

---
+++ frontend/src/assets/icons/Burgermenu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Burgermenu = () => (
+  <div style={{ cursor: 'pointer' }}>
+    <svg xmlns="http://www.w3.org/2000/svg" width="51" height="46" viewBox="0 0 51 46">
+      <defs>
+        <clipPath id="clip-Zeichenfläche_2">
+          <rect width="51" height="46" />
+        </clipPath>
+      </defs>
+      <g id="Zeichenfläche_2" data-name="Zeichenfläche – 2" clipPath="url(#clip-Zeichenfläche_2)">
+        <g id="burgermenu" transform="translate(10 11)">
+          <line id="Linie_1" data-name="Linie 1" x2="24.433" transform="translate(3.567)" fill="none" stroke="#000" strokeLinecap="round" strokeWidth="4" />
+          <line id="Linie_2" data-name="Linie 2" x2="24.433" transform="translate(3.567 21.573)" fill="none" stroke="#000" strokeLinecap="round" strokeWidth="4" />
+          <line id="Linie_3" data-name="Linie 3" x2="28" transform="translate(0 10.787)" fill="none" stroke="#000" strokeLinecap="round" strokeWidth="4" />
+        </g>
+      </g>
+    </svg>
+  </div>
+);
+
+export default Burgermenu;
